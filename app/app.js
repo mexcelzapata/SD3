c4230fec31fde5ad7d12ce02ee32c8e614a6be05
NicoBolton <71084820+nicobolton@users.noreply.github.com>
2021-12-07 03:37:03 -0300

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -1,34 +1,33 @@
 const express = require("express");
 const app = express();
 const port = 4000;
-const Pool = require('pg').Pool;
 
 app.use(express.json());
 
-const master = new Pool({
-    user: "postgres",
-    password: "postgres",
-    host: "PGSQL",
-    port: 5432,
-    database: "postgres"
-})
-const slate = new Pool({
-    user: "postgres",
-    password: "postgres",
-    host: "PGSQL_slave",
-    port: 5432,
-    database: "postgres"
-})
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+      host : 'PGSQL',
+      port : 5432,
+      user : 'postgres',
+      password : 'postgres',
+      database : 'postgres'
+    }
+  });
 
 app.get('/GetProduct/:nombre', async (req, res) => {
-    const query = await slate.query("SELECT * FROM productos WHERE nombre=$1",[req.params.nombre]);
-    console.log("probandoando GetProduct")
-    res.send(query.rows)
+    //const query = await slate.query("SELECT * FROM productos WHERE nombre=$1",[req.params.nombre]);
+    const nom = `%${req.params.nombre}%` 
+    knex('productos').where('nombre', 'like', nom).then(data=>{res.send(data)});
+    
+
+    console.log("probandoando GetProduct ")
+    //res.send(query.rows)
 });
 
 app.post('/AddProduct', async (req, res) => {
     console.log("probandoando AddProduct")
-    await master.query("INSERT INTO productos (nombre, precio) VALUES ($1, $2)",[req.body.nombre,req.body.precio]);
+    knex('productos').returning('*').insert({nombre:req.body.nombre, precio:req.body.precio }).then(data=>{res.send(data)});
     res.send("Se inserto con exito")
 });
 
